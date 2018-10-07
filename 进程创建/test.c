552f79a55f494d4da7eef35846ff06b34c45d504
LiLiLiLaLa <1921300809@qq.com>
2018-10-07 21:52:51 +0800

    联系使用fork函数！

---
+++ 进程创建/test.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+
+int main() {
+    pid_t ret = fork();
+    if(ret<0) {
+        printf("error\n");
+    } else if(ret == 0){
+        while (1){
+            printf("child: pid= %d ppid=%d\n",getpid(),getppid());
+            sleep(1);
+        }
+    } else{
+        while(1){
+            //测试中文注释
+            printf("parent: pid=%d ppid=%d\n",getpid(),getppid());
+            sleep(4);
+        }
+    }
+
+    return 0;
+}
